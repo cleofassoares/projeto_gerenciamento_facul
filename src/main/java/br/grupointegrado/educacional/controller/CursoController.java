6e1689a8d08f7711c8787920be7efd96f05b6dc3
cleofassoares <cleofassoares@gmail.com>
2024-11-20 13:43:59 -0300

    tabela v3 corrigida

---
+++ src/main/java/br/grupointegrado/educacional/controller/CursoController.java
@@ -0,0 +1,4 @@
+package br.grupointegrado.educacional.controller;
+
+public class CursoController {
+}
--- src/main/resources/db/migration/V3__create-table-cursos.sql
+++ src/main/resources/db/migration/V3__create-table-cursos.sql
@@ -1,4 +1,5 @@
 create table cursos(
+    id int not null primary key auto_increment,
     nome varchar(100),
     codigo varchar(20),
     carga_horaria int
